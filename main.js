43ee7a4fda5211e4166cdaf3465b61318689ecf4
Dstemberger <domi.stemberger@me.com>
2024-07-03 17:45:30 +0200

    added console.log for testing

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
+console.log('test');
+
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
